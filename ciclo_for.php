1ea5a878813899d422448d7175ac003a787edbe3
ruizverano <37131132+ruizverano@users.noreply.github.com>
2018-05-14 21:09:07 -0500

    Update ciclo_for.php

--- ciclo_for.php
+++ ciclo_for.php
@@ -1,9 +1,10 @@
 <?php
 for($i = 0; $i <= 10; $i = $i + 5){
 	echo $i . "<br />";
+		for($i = 10; $i >= 1; $i--){
+		echo $i . "<br />";
 }
-for($i = 10; $i >= 1; $i--){
-	echo $i . "<br />";
+
 }
 for($i = 1; $i <= 10; $i++){
 	echo $i . "<br />";
